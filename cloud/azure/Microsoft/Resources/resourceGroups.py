9af397748380907a8e7bbafaf1c5a477818e019e
Peter Bona <peter.bona@msci.com>
2022-10-17 05:55:56 +0200

    Using attributes for reading Azuee objects

--- cloud/azure/Microsoft/Resources/resourceGroups.py
+++ cloud/azure/Microsoft/Resources/resourceGroups.py
@@ -1,12 +1,10 @@
-def as_yaml(azure_resource_group):
-    return { "id": azure_resource_group.id,
-                        "name": azure_resource_group.name,
-                        "type": azure_resource_group.type,
-                        "location": azure_resource_group.location,
-                        "tags": azure_resource_group.tags,
-                        "managed_by": azure_resource_group.managed_by,
-                        "properties":convert_azure_resource_group_properties(azure_resource_group.properties)
-                        }
 
-def convert_azure_resource_group_properties(azure_resource_group_properties):
-    return { "provisioning_state": azure_resource_group_properties.provisioning_state}
+yaml_attributes = ("name", "type", "location")
+
+def as_yaml(azure_resource_group):
+    yaml = {}
+    for yaml_attribute in yaml_attributes:
+        yaml[yaml_attribute] = getattr(azure_resource_group, yaml_attribute)
+    if azure_resource_group.tags:
+        yaml["tags"] = azure_resource_group.tags
+    return yaml
