1042560d824a0137911f9a2b8bb897826f1d9845
Peter Bona <peter.bona@msci.com>
2022-11-13 18:43:53 +0100

    Refactoring: Remove obsolote functions from config.py

--- config.py
+++ config.py
@@ -19,21 +19,10 @@ def open_file_for_write(file):
     """Opens file for writing with utf-8 encoding"""
     return open(file,"w", encoding="utf-8")
 
-def expect_string(dict_var, name, error_message):
-    value = dict_var.get(name)
-    if value is None:
-        raise ValueError(error_message)
-    assert isinstance(value, str)
-    return value
-
 def load2(config_file):
     ruamel_yaml = YAML()
     return ruamel_yaml.load(config_file)
 
 def save2(ostream, yaml) -> None:
-    ruamel_yaml = YAML()
-    ruamel_yaml.dump(yaml, ostream)
-
-def save_yaml(yaml, ostream) -> None:
     ruamel_yaml = YAML()
     ruamel_yaml.dump(yaml, ostream)
