6a17117dfd6a1e9a62ab934c5df56d22458a0aef
Peter Bona <peter.bona@msci.com>
2022-11-13 18:41:34 +0100

    Refactoring: Remove config_test.py

--- cloud/azure/api_test.py
+++ cloud/azure/api_test.py
@@ -2,9 +2,11 @@ import logging
 import pytest
 import api
 import azure_yaml
-import config_test
 
 logging.basicConfig(level=logging.DEBUG)
+
+TEST_SUBSCRIPTION_ID="5ed44b1f-1379-4af2-b7c5-097bbd2e2ee2"
+TEST_RESOURCE_GROUP = "test_resource_group"
 
 @pytest.fixture
 def login():
@@ -12,13 +14,13 @@ def login():
 
 @pytest.fixture
 def resource_group(login):
-    api.delete_resource_group(login, config_test.TEST_SUBSCRIPTION_ID, config_test.TEST_RESOURCE_GROUP)
-    api.update_resource_group(login, config_test.TEST_SUBSCRIPTION_ID, config_test.TEST_RESOURCE_GROUP, resource_group={
+    api.delete_resource_group(login, TEST_SUBSCRIPTION_ID, TEST_RESOURCE_GROUP)
+    api.update_resource_group(login, TEST_SUBSCRIPTION_ID, TEST_RESOURCE_GROUP, resource_group={
             azure_yaml.YAML_AZURE_RESOURCE_LOCATION: "northeurope"
         })
-    yield config_test.TEST_RESOURCE_GROUP
-    api.delete_resource_group(login, config_test.TEST_SUBSCRIPTION_ID, config_test.TEST_RESOURCE_GROUP)
+    yield TEST_RESOURCE_GROUP
+    api.delete_resource_group(login, TEST_SUBSCRIPTION_ID, TEST_RESOURCE_GROUP)
 
 
 def test_resource_group_api(login, resource_group, cli_runner):
-    assert api.check_existence(login, config_test.TEST_SUBSCRIPTION_ID, resource_group), f"{resource_group} shoud exist"
+    assert api.check_existence(login, TEST_SUBSCRIPTION_ID, resource_group), f"{resource_group} shoud exist"
